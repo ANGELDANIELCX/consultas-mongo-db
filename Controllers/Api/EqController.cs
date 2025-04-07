584e816d87022298a1efe2f5617d41a450d3f03f
ANGELDANIELCX <angeldaniuel.ramirezhernadez.gen23@bctis105.edu.mx>
2025-04-07 10:08:08 -0600

    07/04/25

--- Controllers/Api/EqController.cs
+++ Controllers/Api/EqController.cs
@@ -4,8 +4,16 @@ using MongoDB.Driver;
 [ApiController]
 [Route("api/eq")]
 public class EqController : Controller {
-    [httpGet("listar-agencias")]
+    [HttpGet("listar-agencias")]
     public IActionResult ListarAgencias(){
-        return Ok();
+        //Listar todos los registros de la agencia Torres Realty
+       
+        MongoClient client = new MongoClient(CadenasConexion.MONGO_DB);
+        var db = client.GetDatabase("Inmuebles");
+        var collection = db.GetCollection<Inmueble>("RentasVentas");
+
+        var filtro = Builders<Inmueble>.Filter.Eq(x => x.Agencia, "Torres Realty");
+        var lista = collection.Find(filtro).ToList();
+        return Ok(lista);   
     }
 }
